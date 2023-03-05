b582ddd3dfbc8e63363587ebbe14c35177fd8192
Jose Gonzalez <joseg@g.ucla.edu>
2023-03-04 18:18:18 -0800

    Incorporated RandomColors to title

--- MemoryGame.js
+++ MemoryGame.js
@@ -106,3 +106,17 @@ function handleCardClick(event) {
 }
 // when the DOM loads
 createDivsForColors(shuffledColors);
+
+function randomRGB(){
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`
+} 
+
+const letters= document.querySelectorAll('.letter');
+const intervalId= setInterval(function() {
+  for (let letter of letters) {
+      letter.style.color= randomRGB();
+  }
+}, 1000);
