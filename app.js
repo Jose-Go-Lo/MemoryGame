2b2df466254d84a1c64d06d5ce179a03940b3b3b
Jose Gonzalez <joseg@g.ucla.edu>
2023-03-18 16:11:11 -0700

    color change for differentiation

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 const gameContainer = document.getElementById("game");
-let card1= null;
-let card2= null;
-let cardsFlipped= 0;
-let nothingClicked= false;
+let card1 = null;
+let card2 = null;
+let cardsFlipped = 0;
+let nothingClicked = false;
 
 const COLORS = [
-  "SaddleBrown",
-  "SeaGreen",
+  "Crimson",
+  "LightPink",
+  "DarkOrange",
+  "Gold",
+  "DarkMagenta",
+  "DarkCyan",
+  "DarkTurquoise",
+  "Sienna",
+  "White",
   "Silver",
-  "SlateGray",
-  "SpringGreen",
-  "Salmon",
-  "SeaShell",
-  "SkyBlue",
-  "SteelBlue",
-  "Thistle",
-  "SaddleBrown",
-  "SeaGreen",
+  "Crimson",
+  "LightPink",
+  "DarkOrange",
+  "Gold",
+  "DarkMagenta",
+  "DarkCyan",
+  "DarkTurquoise",
+  "Sienna",
+  "White",
   "Silver",
-  "SlateGray",
-  "SpringGreen",
-  "Salmon",
-  "SeaShell",
-  "SkyBlue",
-  "SteelBlue",
-  "Thistle",
 ];
 
 // here is a helper function to shuffle an array
@@ -72,61 +72,60 @@ function createDivsForColors(colorArray) {
   }
 }
 
-
 function handleCardClick(event) {
   // you can use event.target to see which element was clicked
   console.log("you just clicked", event.target);
   if (nothingClicked) return;
-  if (event.target.classList.contains('flipped')) return;
+  if (event.target.classList.contains("flipped")) return;
 
-  let clickedCard= event.target;
-  clickedCard.style.backgroundColor= clickedCard.classList[0];
+  let clickedCard = event.target;
+  clickedCard.style.backgroundColor = clickedCard.classList[0];
 
-  if (!card1 || !card2){
-    clickedCard.classList.add('flipped');
-    card1= card1 || clickedCard;
-    card2= clickedCard === card1 ? null : clickedCard;
+  if (!card1 || !card2) {
+    clickedCard.classList.add("flipped");
+    card1 = card1 || clickedCard;
+    card2 = clickedCard === card1 ? null : clickedCard;
   }
 
-  if (card1 && card2){
-    nothingClicked= true;
-    let face1= card1.className;
-    let face2= card2.className;
+  if (card1 && card2) {
+    nothingClicked = true;
+    let face1 = card1.className;
+    let face2 = card2.className;
 
-    if (face1 === face2){
+    if (face1 === face2) {
       cardsFlipped += 2;
-      card1.removeEventListener('click', handleCardClick);
-      card2.removeEventListener('click', handleCardClick);
-      card1= null;
-      card2= null;
-      nothingClicked= false;
+      card1.removeEventListener("click", handleCardClick);
+      card2.removeEventListener("click", handleCardClick);
+      card1 = null;
+      card2 = null;
+      nothingClicked = false;
     } else {
-      setTimeout(function(){
-        card1.style.backgroundColor= '';
-        card2.style.backgroundColor= '';
-        card1.classList.remove('flipped');
-        card2.classList.remove('flipped');
-        card1= null;
-        card2= null;
-        nothingClicked= false;
+      setTimeout(function () {
+        card1.style.backgroundColor = "";
+        card2.style.backgroundColor = "";
+        card1.classList.remove("flipped");
+        card2.classList.remove("flipped");
+        card1 = null;
+        card2 = null;
+        nothingClicked = false;
       }, 750);
     }
-  } 
-  if (cardsFlipped=== COLORS.length) alert('YOU WIN!')
+  }
+  if (cardsFlipped === COLORS.length) alert("YOU WIN!");
 }
 // when the DOM loads
 createDivsForColors(shuffledColors);
 
-function randomRGB(){
+function randomRGB() {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
-  return `rgb(${r}, ${g}, ${b})`
-} 
+  return `rgb(${r}, ${g}, ${b})`;
+}
 
-const letters= document.querySelectorAll('.letter');
-const intervalId= setInterval(function() {
+const letters = document.querySelectorAll(".letter");
+const intervalId = setInterval(function () {
   for (let letter of letters) {
-      letter.style.color= randomRGB();
+    letter.style.color = randomRGB();
   }
 }, 1000);
